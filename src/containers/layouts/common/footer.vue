e84639a83d1afe549ba2d021deefedf44b56b2a9
Miguel AB <abm_8249@outlook.com>
2021-02-28 17:46:14 -0500

    instalar vue y tema

---
+++ src/containers/layouts/common/footer.vue
@@ -0,0 +1,42 @@
+ <template>
+  <div class="footer_wrap">
+    <!-- Footer Start -->
+    <div class="flex-grow-1"></div>
+    <div class="app-footer">
+      <div class="row">
+        <div class="col-md-9">
+          <p><strong>Caja Arequipa</strong></p>
+        </div>
+      </div>
+      <div
+        class="footer-bottom border-top pt-3 d-flex flex-column flex-sm-row align-items-center"
+      >
+        <div class="d-flex align-items-center">
+          <img class="logo" src="@/assets/images/logo.png" alt="" />
+          <div>
+            <div>
+              <p class="m-0">&copy; {{year}}</p>
+              <p class="m-0">All rights reserved</p>
+            </div>
+          </div>
+          <span class="flex-grow-1"></span>
+        </div>
+      </div>
+      <!-- fotter end -->
+    </div>
+  </div>
+</template> 
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  },
+  methods: {}
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style> 
